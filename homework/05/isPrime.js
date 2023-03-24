59ff6ed215269f11c35139faa20175599ea089b4
921006vicky <s111110505@student.nqu.edu.tw>
2023-03-25 00:02:11 +0800

    isPrime.js

--- homework/05/isPrime.js
+++ homework/05/isPrime.js
@@ -1,13 +1,17 @@
 function isPrime(n) {
     if (n <= 1) {
         return false
-    }
-    for (var c = 2; c <= Math.sqrt(n); c++) {
+    }//先排除0和1
+    for (let c = 2; c <= Math.sqrt(n); c++) {
         if (n % c === 0) {
             return false
         }
+        //並且只需要判斷到平方根的整數部分即可，因為如果存在一個大於平方根的因數，那麼必定存在一個小於平方根的因數，這樣就可以減少不必要的計算量。
     }
     return true
 }
 
 console.log('isPrime(101)', isPrime(101)) //'isPrime(101)',不加也沒事
+
+
+//let用於宣告的區域，var用於所有區域 
