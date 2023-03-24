6e224f6aad926e27cc2105c97399d6412ad4efd3
921006vicky <s111110505@student.nqu.edu.tw>
2023-03-24 11:07:00 +0800

    isPrime

--- homework/05/isPrime.js
+++ homework/05/isPrime.js
@@ -1,22 +1,20 @@
-function isPrime(n) {
-    // 2 是質數
-    if (n === 2) {
-      return true;
+function isPrime(n)
+{
+    if(n<=1)
+    {
+        return false
     }
-    
-    // 若 n 為小於 2 或偶數，不是質數
-    if (n < 2 || n % 2 === 0) {
-      return false;
+    for(var c=2;c<=Math.sqrt(n);c++)
+    {
+        if(n%c===0)
+        {
+            return false
+        }
     }
-    
-    // 從 3 開始，每次增加 2，只需檢查到根號 n 即可
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
-      if (n % i === 0) {
-        return false;
-      }
-    }
-    
-    // 若無法整除，則為質數
-    return true;
-    
-  }
+    return true
+}
+
+console.log('isPrime(1)=',isPrime(1))
+console.log('isPrime(2)=',isPrime(2))
+console.log('isPrime(3)=',isPrime(3))
+console.log('isPrime(4)=',isPrime(4))
